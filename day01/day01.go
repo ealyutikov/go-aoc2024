e48f6191ff51e9ea9e5e3624972747d4ac91c771
ealyutikov <ealyutikov@gmail.com>
2024-12-05 12:14:46 +0300

    day01

--- day01/day01.go
+++ day01/day01.go
@@ -1,1 +1,36 @@
 package day01
+
+import "slices"
+
+func solutionA(listA, listB []int) (result int) {
+	slices.Sort(listA)
+	slices.Sort(listB)
+
+	for i := 0; i < len(listA); i++ {
+		result += diff(listA[i], listB[i])
+	}
+
+	return result
+}
+
+func solutionB(listA, listB []int) (result int) {
+	for a := 0; a < len(listA); a++ {
+		count := 0
+		for b := 0; b < len(listB); b++ {
+			if listA[a] == listB[b] {
+				count++
+			}
+		}
+
+		result += listA[a] * count
+	}
+
+	return result
+}
+
+func diff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -2,12 +2,16 @@ package day01
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"os"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var input []string
+var inputL, inputR []int
 
 func TestMain(m *testing.M) {
 	file, err := os.Open("input.txt")
@@ -18,11 +22,35 @@ func TestMain(m *testing.M) {
 	}
 
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
+	scanner.Split(bufio.ScanWords)
 
+	i := 0
 	for scanner.Scan() {
-		input = append(input, scanner.Text())
+		res, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (i % 2) == 0 {
+			inputL = append(inputL, res)
+		} else {
+			inputR = append(inputR, res)
+		}
+
+		i++
+	}
+
+	if len(inputL) != len(inputR) {
+		log.Fatal(errors.New("inputL != inputR"))
 	}
 
 	m.Run()
+}
+
+func TestSolutionA(t *testing.T) {
+	assert.Equal(t, 2769675, solutionA(inputL, inputR))
+}
+
+func TestSolutionB(t *testing.T) {
+	assert.Equal(t, 24643097, solutionB(inputL, inputR))
 }
